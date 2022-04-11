6823c61a92882ff9c90c73a7b872fba2d3b5a1a2
Lynniswaifu <lynniswaifu@gmail.com>
2022-04-11 12:42:41 +0700

    Update myFunction.py

--- src/cogs/customFunction/myFunction.py
+++ src/cogs/customFunction/myFunction.py
@@ -1,9 +1,5 @@
 import time
 import nextcord
-import bs4
-from numpy import mat
-from selenium import webdriver
-
 
 def embed_msg(desc):
     embed = nextcord.Embed(title="Ritsu Helper", description=desc)
