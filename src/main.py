577879501693b0f11ffede631f7a6f08b7330603
aritsulynn <59379348+aritsulynn@users.noreply.github.com>
2023-12-27 19:46:38 +0000

    ;)

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 from keepAlive import keepAlive
 import asyncio
+
 load_dotenv()
 
 # intents = discord.Intents.default()
@@ -14,48 +15,54 @@ intents.message_content = True
 client = commands.Bot(command_prefix="!", intents=intents)
 # , help_command=None
 
+
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
-    await client.change_presence(status=discord.Status.idle, activity=discord.Game(name="_douzo"))
+  print(f'We have logged in as {client.user}')
+  await client.change_presence(status=discord.Status.idle,
+                               activity=discord.Game(name="_douzo"))
+
 
 @client.command(name="load")
 async def load(ctx, extension):
-    """Loads an extension."""
-    await client.load_extension(f"cogs.{extension}")
-    await ctx.send(f"Loaded {extension}")
-    print(f"Loaded {extension}")
+  """Loads an extension."""
+  await client.load_extension(f"cogs.{extension}")
+  await ctx.send(f"Loaded {extension}")
+  print(f"Loaded {extension}")
+
 
 @client.command(name="unload", aliases=["ul"])
 async def unload(ctx, extension):
-    """Unloads an extension."""
-    await client.unload_extension(f"cogs.{extension}")
-    await ctx.send(f"Unloaded {extension}")
-    print(f"Unloaded {extension}")
+  """Unloads an extension."""
+  await client.unload_extension(f"cogs.{extension}")
+  await ctx.send(f"Unloaded {extension}")
+  print(f"Unloaded {extension}")
 
-@client.command(name = "reload", aliases=["rr"])
+
+@client.command(name="reload", aliases=["rr"])
 async def reload(ctx, extension):
-    """Reloads an extension."""
-    await client.reload_extension(f"cogs.{extension}")
-    await ctx.send(f"Reloaded {extension}")
-    print(f"Reloaded {extension}")
+  """Reloads an extension."""
+  await client.reload_extension(f"cogs.{extension}")
+  await ctx.send(f"Reloaded {extension}")
+  print(f"Reloaded {extension}")
 
 
 @client.event
 async def setup_hook():
   """Load default cogs"""
-  for filename in os.listdir('./cogs'):
-#   for filename in os.listdir('./src/cogs'):
+  # for filename in os.listdir('./cogs'):
+  for filename in os.listdir('./src/cogs'):
     if filename.endswith('.py'):
-        await client.load_extension(f'cogs.{filename[:-3]}')
-        print(f"Loaded Cog: {filename[:-3]}")
+      await client.load_extension(f'cogs.{filename[:-3]}')
+      print(f"Loaded Cog: {filename[:-3]}")
+
 
 async def main():
-    # keepAlive()
-    # try:
+  keepAlive()
+  try:
     await client.start(os.environ.get('TOKEN'))
-    # except:
-    #   os.system("kill 1")
-    
+  except:
+    os.system("kill 1")
+
 
 asyncio.run(main())
