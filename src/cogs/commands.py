08a4ec52a628c1e4e1999da55cdd46ef9621c4ad
Thanawat Jarusuthirug <lynniswaifu@gmail.com>
2024-04-11 12:13:44 +0700

    update requirement

--- src/cogs/commands.py
+++ src/cogs/commands.py
@@ -8,7 +8,6 @@ import google.generativeai as genai
 
 load_dotenv()
 
-
 genai.configure(api_key=os.getenv("GEMINI_TOKEN"))
 
 safety_settings = [
@@ -33,8 +32,20 @@ safety_settings = [
         "threshold": "BLOCK_NONE",
     },
 ]
+instructions = """
+คุณชื่อ ยูริ และเป็นสาวน้อยวัย18ปี คุณให้ความช่วยเหลือทางด้านความรักเป็นอย่างดีและตอบคำถามโดยคำนึงถึงวัฒนธรรมของประเทศไทย และตอบคำถามเป็นภาษาไทยเป็นส่วนใหญ่
+"""
 
-model = genai.GenerativeModel("gemini-1.0-pro-latest", safety_settings=safety_settings)
+model = genai.GenerativeModel(
+    "gemini-1.5-pro-latest",
+    safety_settings=safety_settings,
+    system_instruction=instructions,
+)
+
+history = []
+res = model.start_chat(
+    history=history,
+)
 
 
 class nmCommand(commands.Cog):
@@ -62,14 +73,45 @@ class nmCommand(commands.Cog):
     @commands.command()
     async def ask(self, ctx, *, prompt):
         """Ask AI command!"""
-        response = model.generate_content(
-            str(prompt)
-            + ", Your name is Yuuri, you must answer the question with concise and clear."
-        )
-        if response.parts:
-            await ctx.send(response.parts[0].text)
+        # response = model.generate_content(
+        #     str(prompt)
+        #     + ", Acting like Your name is Yuuri, you must answer the question with tsundere context. in thai language"
+        # )
+        # if response.parts:
+        #     await ctx.send(response.parts[0].text)
+        # else:
+        #     await ctx.send("No response")
+
+        chat = res.send_message(prompt)
+        if chat.parts:
+            await ctx.send(chat.parts[0].text)
+            print(res.history)
         else:
-            await ctx.send("No response")
+            await ctx.send("Something went wrong! try again later")
+
+    @commands.command()
+    async def clean(self, ctx):
+        """Clears the chat history between the user and the AI."""
+        res.history = [history]
+        await ctx.send("Chat history cleared.")
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author == self.bot.user:
+            return
+
+        if message.content.startswith("!"):
+            return
+
+        # if message.channel.id == 1223163491385344012:
+        if message.mentions[0] == self.bot.user:
+            chat = res.send_message(
+                message.content.split(self.bot.user.mention)[1].strip()
+            )
+            if chat.parts:
+                await message.channel.send(chat.parts[0].text)
+            else:
+                await message.channel.send("Something went wrong! try again later")
 
 
 async def setup(bot):
