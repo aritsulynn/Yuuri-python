2da53e17be7cd3bf20200d8cc39c300656fa6081
Thanawat Jarusuthirug <lynniswaifu@gmail.com>
2024-07-19 02:25:43 +0700

    upgrade gem

--- src/cogs/gemai.py
+++ src/cogs/gemai.py
@@ -19,9 +19,15 @@ safety_settings = {
 
 
 model = genai.GenerativeModel(
-    "gemini-1.5-pro-latest",
+    "gemini-1.5-flash-latest",
     safety_settings=safety_settings,
     system_instruction=os.getenv("AI_INSTRUCTIONS"),
+    # generation_config={
+    #     "temperature": 0.6,
+    #     "max_output_tokens": 1024,
+    #     "candidate_count": 1,
+    #     "stop_sequences": ["\n"],
+    # },
 )
 
 history = []
@@ -41,15 +47,27 @@ class gemAI(commands.Cog):
         if message.content.startswith("!"):
             return
 
-        if message.mentions[0] == self.bot.user:
+        # if channel is 1223163491385344012 it will be responded to by the AI
+        # print(message.channel.id)
+
+        if (
+            message.channel.id == 1223163491385344012
+            or message.mentions[0] == self.bot.user
+        ):
             prompt = (
-                f"{message.author.name} : "
-                + message.content.split(self.bot.user.mention)[1].strip()
+                f"{message.author.name} : {message.content.split(self.bot.user.mention)[1].strip()}"
+                if self.bot.user in message.mentions
+                else message.content
             )
             async with message.channel.typing():
                 res = chat.send_message(prompt)
                 if res.parts:
-                    await message.channel.send(res.parts[0].text)
+                    text = res.parts[0].text
+                    if len(text) > 2000:
+                        for i in range(0, len(text), 2000):
+                            await message.channel.send(text[i : i + 2000])
+                    else:
+                        await message.channel.send(text)
                 else:
                     await message.channel.send("Something went wrong! Try again later.")
 
