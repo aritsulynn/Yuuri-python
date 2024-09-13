91ae97dd25eed02f974140fcc0a394c4a84ba231
lynn <lynniswaifu@gmail.com>
2024-09-13 18:49:31 +0700

    fix err

--- src/cogs/yuuAI.py
+++ src/cogs/yuuAI.py
@@ -5,15 +5,19 @@ from datetime import datetime
 from dotenv import load_dotenv
 import requests
 import re
+from upstash_redis import Redis
 
 load_dotenv()
 API_URL = "http://flowise:3001/api/v1/prediction/55927f33-3576-4755-9eb4-5438ddf6915b"
+# API_URL = "https://flowise.9lynn.com/api/v1/prediction/55927f33-3576-4755-9eb4-5438ddf6915b"
 headers = {"Authorization": f"Bearer {os.getenv("BEARER_TOKEN")}"}
 
 class yuuAI(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.redis = Redis(url=os.getenv("UPSTASH_REDIS_REST_URL"), token=os.getenv("UPSTASH_REDIS_REST_TOKEN"))
+        print(self.redis.ping())
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -29,7 +33,7 @@ class yuuAI(commands.Cog):
 
 
         if (
-            message.channel.id == 1223163491385344012
+            message.channel.id in [1223163491385344012]
             or message.mentions[0] == self.bot.user
         ):
             
@@ -56,8 +60,13 @@ class yuuAI(commands.Cog):
                     await message.channel.send("Something went wrong! Try again later.")
 
     # reset chat session
-    # @commands.command()
-    # async def reset(self, ctx):
+    @commands.command()
+    async def reset(self, ctx):
+        status = self.redis.delete(str(ctx.author.id))
+        if status:
+            await ctx.send("Chat session reset successfully!")
+        else:
+            await ctx.send("Chat session not found!")
 
 
 async def setup(bot):
