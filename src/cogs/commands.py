76dd4b2f0e906d11779cc00790ffc96c8cbc1dd5
lynn <lynniswaifu@gmail.com>
2024-09-14 19:56:47 +0700

    interaction

--- src/cogs/commands.py
+++ src/cogs/commands.py
@@ -4,24 +4,11 @@ import discord
 from discord.ext import commands
 from datetime import datetime
 
-# from pymongo.mongo_client import MongoClient
-# from pymongo.server_api import ServerApi
-
 
 class nmCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        # self.client = MongoClient("your-mongodb-connection-string")
-        # self.db = self.client["mydatabase"]
-        # self.collection = self.db["mycollection"]
-        # self.client = MongoClient(os.getenv("mongodbURI"), server_api=ServerApi("1"))
-        # self.db = self.client["yuu-test"]
-        # self.collection = self.db["yuu-collection"]
-        # try:
-        #     self.client.admin.command("ping")
-        #     print("ping mongo")
-        # except Exception as e:
-        #     print("failed to ping")
+
 
     @commands.command()
     async def ping(self, ctx):
@@ -40,30 +27,6 @@ class nmCommand(commands.Cog):
         self.bot.tree.clear_commands(guild=ctx.guild)
         await self.bot.tree.sync()
         await ctx.send("Desynced")
-
-    @commands.command(name="random")
-    async def random(self, ctx):
-        print("random")
-        list = ["A", "B", "C"]
-        await ctx.reply(f"I choose {random.choice(list)}")
-
-    # @commands.command(name="test")
-    # async def test(self, ctx):
-    #     user_data = {"user_id": ctx.author.id, "user_name": ctx.author.name}
-    #     self.collection.insert_one(user_data)
-    #     await ctx.send(f"User {user_data} added to the database.")
-
-    # @commands.command()
-    # async def ask(self, ctx, *, prompt):
-    #     """Ask AI command!"""
-    #     # Get or create a chat instance for the context
-    #     chat = self.get_chat_instance(ctx)
-    #     # Send message to AI and get response
-    #     response = chat.send_message(prompt)
-    #     if response.parts:
-    #         await ctx.send(response.parts[0].text)
-    #     else:
-    #         await ctx.send("Something went wrong! Try again later.")
 
 
 async def setup(bot):
--- src/cogs/slashCommand.py
+++ src/cogs/slashCommand.py
@@ -3,13 +3,28 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 from dotenv import load_dotenv
+import requests
+import re
+from upstash_redis import Redis
+
 
 load_dotenv()
+API_URL = "http://flowise:3001/api/v1/prediction/55927f33-3576-4755-9eb4-5438ddf6915b"
+# API_URL = (
+#     "https://flowise.9lynn.com/api/v1/prediction/55927f33-3576-4755-9eb4-5438ddf6915b"
+# )
+headers = {"Authorization": f"Bearer {os.getenv("BEARER_TOKEN")}"}
+
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
 
 
 class slashCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.redis = Redis(url=os.getenv("UPSTASH_REDIS_REST_URL"), token=os.getenv("UPSTASH_REDIS_REST_TOKEN"))
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -21,15 +36,29 @@ class slashCommand(commands.Cog):
             f"Pong! {round(self.bot.latency * 1000)}ms", ephemeral=True
         )
 
-    @app_commands.command(name="profile", description="A cool profile command!")
-    async def profile(self, interaction: discord.Interaction):
-        """A cool profile command!"""
-        embed = discord.Embed(
-            title="Profile",
-            description="This is a profile command!",
-            color=discord.Color.random(),
+    @app_commands.command(name="ask", description="Ask a question!")
+    async def ask(self, interaction: discord.Interaction, question: str):
+        """Ask AI command!"""
+        await interaction.response.defer(ephemeral=True)
+        output = query(
+            {
+                "question": question,
+                "overrideConfig": {
+                    "sessionId": f"{interaction.user.id}",
+                },
+            }
         )
+        print(interaction.user.name + " asked: " + question)
+        await interaction.followup.send(output["text"], ephemeral=True)
 
+    @app_commands.command(name="reset", description="Reset the chat session!")
+    async def reset(self, interaction: discord.Interaction):
+        await interaction.response.defer(ephemeral=True)
+        status = self.redis.delete(str(interaction.user.id))
+        if status:
+            await interaction.followup.send("Chat session reset successfully!")
+        else:
+            await interaction.followup.send("Chat session not found!")
 
 async def setup(bot):
     await bot.add_cog(
